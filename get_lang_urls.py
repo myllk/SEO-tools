0453c3782a98fea6bad53a3c7d73dbaa55b7c618
James Rhymes <jamesrhym@gmail.com>
2019-01-15 18:17:16 +0000

    added csv support

--- get_lang_urls.py
+++ get_lang_urls.py
@@ -4,8 +4,9 @@ import re
 
 # arg setup
 parser = argparse.ArgumentParser(description='Return all the URLS in a file that contain english words')
-parser.add_argument('-d', '--domain', help='The full domain i.e "https://www.google.com" (this text will be ignored in the search)', default="")
-parser.add_argument('-l', '--lang', help='The language for pyenchant to use', default="en_UK")
+parser.add_argument('-d', '--domain', help='The full domain i.e "https://www.google.com" (this text will be ignored in the search), default: ""', default="")
+parser.add_argument('-l', '--lang', help='The language for pyenchant to use, default: en_UK', default="en_UK")
+parser.add_argument('-o', '--output_type', help='The output file type (txt or csv), default: csv', default="csv")
 parser.add_argument('file', help='Path to the file containing the URLs')
 args = parser.parse_args()
 
@@ -16,18 +17,33 @@ print("filename: "+args.file)
 print("language: "+args.lang)
 
 # output file names
-lang_output_name = args.file.split('.')[0]+"_"+args.lang+".txt"
-other_output_name = args.file.split('.')[0]+"_other.txt"
-print("matched lines will be appended to: "+lang_output_name)
-print("unmatched lines will be appended to: "+other_output_name)
+if args.output_type == "txt":
+    lang_output_name = args.file.split('.')[0]+"_"+args.lang+".txt"
+    other_output_name = args.file.split('.')[0]+"_other.txt"
+    print("matched lines will be appended to: "+lang_output_name)
+    print("unmatched lines will be appended to: "+other_output_name)
+
+elif args.output_type == "csv":
+    output_name = args.file.split('.')[0]+"_"+args.lang+".csv"
+    print("Lines will be appended to: "+output_name)
+
+else:
+    print("Invalid output type. Type must be txt or csv")
+    exit(1)
 
 # banned chars and create dict obj
 banned_chars = [r'\/', r'\.', r'-', r'_', r':']
 d = enchant.Dict(args.lang)
 
 # open output files
-lang_output = open(lang_output_name, "w+")
-other_output = open(other_output_name, "w+")
+if args.output_type == "txt":
+    lang_output = open(lang_output_name, "w+")
+    other_output = open(other_output_name, "w+")
+
+elif args.output_type == "csv":
+    output = open(output_name, "w+")
+    output.write("url,"+args.lang+"?\n")
+
 
 # counters for stats
 i = 0
@@ -57,20 +73,29 @@ with open(args.file) as f:
             # if any word in the line is a valid word in chosen lang
             if d.check(word):
                 # add to lang file, increment counter and stop working on this line
-                lang_output.write(org_line)
+                if args.output_type == "txt":
+                    lang_output.write(org_line)
+                elif args.output_type == "csv":
+                    output.write(org_line.rstrip("\n\r")+",yes\n")
                 found_lang = True
                 i_matched += 1
                 break
 
         # if no matches found, add to unmatched file and increment counter
         if not found_lang:
-            other_output.write(org_line)
+            if args.output_type == "txt":
+                other_output.write(org_line)
+            elif args.output_type == "csv":
+                output.write(org_line.rstrip("\n\r")+",no\n")
             i_unmatched += 1
 
         i += 1
 
-lang_output.close()
-other_output.close()
+if args.output_type == "txt":
+    lang_output.close()
+    other_output.close()
+elif args.output_type == "csv":
+    output.close()
 
 print("Finished working on "+str(i)+" lines")
 print(str(i_matched)+" lines matched the language "+args.lang)
