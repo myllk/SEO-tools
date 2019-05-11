19d4c497b5a564b7ec429546dde29a11653bcc4b
James Rhymes <jamesrhym@gmail.com>
2019-05-11 17:52:29 +0100

    add check_urls_for_phrase.py

---
+++ check_urls_for_phrase.py
@@ -0,0 +1,19 @@
+import csv
+
+phrase_file = "check_urls_for_phrase/URL List - Locations.csv"
+check_file = "check_urls_for_phrase/URL List - URLs.csv"
+
+with open(check_file) as check_csv:
+    check_file_reader = csv.reader(check_csv)
+
+    for check_line in check_file_reader:
+        # print(check_line[0])
+
+        with open(phrase_file) as phrase_csv:
+            phrase_csv_reader = csv.reader(phrase_csv)
+
+            for phrase in phrase_csv_reader:
+                # print(phrase[0].lower())
+
+                if phrase[0].lower() in check_line[0]:
+                    print(phrase[0] + ", " + check_line[0])
