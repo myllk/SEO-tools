aefe1364991facc63e69f9f60529b05d80af7447
James Rhymes <jamesrhym@gmail.com>
2019-11-17 16:58:37 +0000

    removed debug

--- check_urls_for_phrase.py
+++ check_urls_for_phrase.py
@@ -7,13 +7,10 @@ with open(check_file) as check_csv:
     check_file_reader = csv.reader(check_csv)
 
     for check_line in check_file_reader:
-        # print(check_line[0])
 
         with open(phrase_file) as phrase_csv:
             phrase_csv_reader = csv.reader(phrase_csv)
 
             for phrase in phrase_csv_reader:
-                # print(phrase[0].lower())
-
                 if phrase[0].lower() in check_line[0]:
                     print(phrase[0] + ", " + check_line[0])
